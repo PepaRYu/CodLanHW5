7068c51155cbf9c3a154df40819109233305ae80
DrPRun <sheighd@gmail.com>
2022-08-10 00:35:36 +0300

    zadacha 38 reshena

---
+++ Zadacha038/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+int max, min;
+max = min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (max < array[i]) max = array[i];
+    if (min > array[i]) min = array[i];
+}
+Console.WriteLine($"Выводим массив на экран $ {String.Join(", ", array)}");
+Console.WriteLine($"Максимальное значение массива равно {max}; \nМинимальное значение массива равно {min}");
