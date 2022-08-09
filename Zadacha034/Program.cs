54ceee82965508c3e851db5ba232a8acc2ebadb2
DrPRun <sheighd@gmail.com>
2022-08-09 23:46:59 +0300

    Zadacha 34 sdelana

---
+++ Zadacha034/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите размер массива");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+Console.Write($"Массив: {String.Join(", ", array)} \n");
+int counter = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2 == 0)
+    {
+        counter ++;
+    }
+}
+Console.WriteLine($"Количество чётных элементов в массиве равно: {counter}");
